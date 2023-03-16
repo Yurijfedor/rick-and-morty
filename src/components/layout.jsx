8b4d9ffbac7c95d4bf1740e1d25d906bce94ea36
Yurijfedor <yurijfedor178@gmail.com>
2023-03-16 12:55:34 +0200

    replace useeffect

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,17 +1,24 @@
 import { Outlet } from "react-router-dom";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchAllCharacters } from "../redux/operations";
 import { Container } from "./Layout.styled";
 import { AuthNav } from "./AuthNav";
 import { LogOut } from "./LogOut";
 import { UseAuth } from "../hooks/useAuth";
 
 const Layout = () => {
+  const dispatch = useDispatch();
   const { isAuth } = UseAuth();
+
+  useEffect(() => {
+    dispatch(fetchAllCharacters());
+  }, [dispatch]);
 
   return (
     <Suspense>
       <Container>
-        {isAuth ? <LogOut/> : <AuthNav />}
+        {isAuth ? <LogOut /> : <AuthNav />}
         <Outlet fallback={<div>...Loading</div>} />
       </Container>
     </Suspense>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,20 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+// import { useEffect } from "react";
+// import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/logo";
 import { Filter } from "../components/Filter";
 import { CharactersList } from "../components/ChractersList";
-import { fetchAllCharacters } from "../redux/operations";
+// import { fetchAllCharacters } from "../redux/operations";
 import { UseAuth } from "../hooks/useAuth";
 
 const Characters = () => {
   const { isAuth } = UseAuth();
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    dispatch(fetchAllCharacters());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(fetchAllCharacters());
+  // }, [dispatch]);
 
   return isAuth ? (
     <>
